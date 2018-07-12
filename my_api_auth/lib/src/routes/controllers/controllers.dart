a39201f8c81cc391b04c0f112d66e252f4fb4a94
Tobe O <thosakwe@gmail.com>
2018-07-12 11:00:34 -0400

    Removed unused boilerplate files

--- my_api_auth/lib/src/routes/controllers/controllers.dart
+++
@@ -1,8 +0,0 @@
-library my_api_auth.src.routes.controllers;
-
-import 'dart:async';
-import 'package:angel_framework/angel_framework.dart';
-
-Future configureServer(Angel app) async {
-  /// Controllers will not function unless wired to the application!
-}
--- my_api_auth/lib/src/routes/routes.dart
+++ my_api_auth/lib/src/routes/routes.dart
@@ -1,10 +1,8 @@
 /// This app's route configuration.
 library my_api_auth.src.routes;
 
-import 'package:angel_cors/angel_cors.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:file/file.dart';
-import 'controllers/controllers.dart' as controllers;
 
 /// Put your app routes here!
 ///
@@ -13,12 +11,6 @@ import 'controllers/controllers.dart' as controllers;
 /// * https://github.com/angel-dart/angel/wiki/Requests-&-Responses
 AngelConfigurer configureServer(FileSystem fileSystem) {
   return (Angel app) async {
-    // Enable CORS
-    app.use(cors());
-
-    // Typically, you want to mount controllers first, after any global middleware.
-    await app.configure(controllers.configureServer);
-
     // Throw a 404 if no route matched the request.
     app.use(() => throw new AngelHttpException.notFound());
   };
